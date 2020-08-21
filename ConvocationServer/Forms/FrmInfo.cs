b1c82d460af508f52fcdef872f05eeaa43834c91
James <james@jmew.ca>
2020-08-21 12:24:25 -0400

    Add information form

---
+++ ConvocationServer/Forms/FrmInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ConvocationServer.Forms
+{
+    public partial class FrmInfo : Form
+    {
+        private readonly FrmServer _parent;
+
+        public FrmInfo(FrmServer parent)
+        {
+            InitializeComponent();
+            _parent = parent;
+
+            Text = $"{_parent.Text} - Information";
+
+            FileVersionInfo version = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
+
+            lblName.Text = version.ProductName;
+            lblVersion.Text = version.FileVersion;
+            lblCompany.Text = version.CompanyName;
+            lblCopyright.Text = version.LegalCopyright;
+        }
+
+        private void btnCancel_Click(object sender, System.EventArgs e)
+        {
+            Dispose();
+        }
+    }
+}
